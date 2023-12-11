524fc69083b824ebe484b8c27f1a94e4cd4b56b4
AnugerahR08 <anugerahrama04@gmail.com>
2023-12-11 23:47:54 +0700

    add seeder admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,66 +22,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Fitra Fajar',
-            'username' => 'fitra',
-            'level' => 'admin',
-            'email' => 'fitra@gmail.com',
-            'password' => bcrypt('12345')
+        $this->call([
+            UserSeeder::class,
         ]);
-        
-        User::factory(3)->create();
-
-
-        Category::create([
-            'name' => 'Web Programming',
-            'slug' => 'web-programming'
-        ]);
-
-        Category::create([
-            'name' => 'Web Design',
-            'slug' => 'web-design'
-        ]);
-
-        Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        Post::factory(10)->create();
-        Anak::factory(10)->create();
-        Ortu::factory(10)->create();
-        JenisImunisasi::factory(3)->create();
-        Imunisasi::factory(10)->create();
-        BukuTamu::factory(5)->create();
-        VitaminA::factory(5)->create();
-        
-        // Post::create([
-        //     'title' => 'Judul Pertama',
-        //     'slug' => 'judul-pertama',
-        //     'excerpt' => 'lorem ipsum pertama',
-        //     'body' => 'lorem ipsum dolor sit amet',
-        //     'category_id' => 1,
-        //     'user_id' => 1
-        // ]);
-        
-        // Post::create([
-        //     'title' => 'Judul Kedua',
-        //     'slug' => 'judul-kedua',
-        //     'excerpt' => 'lorem ipsum kedua',
-        //     'body' => 'lorem ipsum dolor sit amet',
-        //     'category_id' => 1,
-        //     'user_id' => 1
-        // ]);
-
-        // Post::create([
-        //     'title' => 'Judul Ketiga',
-        //     'slug' => 'judul-ketiga',
-        //     'excerpt' => 'lorem ipsum ketiga',
-        //     'body' => 'lorem ipsum dolor sit amet',
-        //     'category_id' => 2,
-        //     'user_id' => 1
-        // ]);
-
     }
 }
