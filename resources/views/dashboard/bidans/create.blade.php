bf6424a704b375e341b21c77d1e7a2c905970811
AnugerahR08 <anugerahrama04@gmail.com>
2023-12-11 23:41:03 +0700

    repair bidans

--- resources/views/dashboard/bidans/create.blade.php
+++ resources/views/dashboard/bidans/create.blade.php
@@ -1,119 +1,120 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Tambah Data Bidan</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
-            <li class="breadcrumb-item active">Tambah Bidan</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
-
-  <!-- Main content -->
-  <section class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <!-- left column -->
-        <div class="col-md-12">
-          <!-- general form elements -->
-          <div class="card card-primary">
-            <div class="card-header">
-              <h3 class="card-title">Tambah Bidan</h3>
-            </div>
-            <!-- /.card-header -->
-            <!-- form start -->
-            <form method="post" action="{{ route('bidans.store') }}" class="mb-5" enctype="multipart/form-data">
-              @csrf
-              <div class="card-body">
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-md-12">
-                            <label for="nama_bidan">Nama bidan</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
-                                </div>
-                                <input type="text" class="form-control @error('nama_bidan') is-invalid @enderror" id="nama_bidan" name="nama_bidan" value="{{ old('nama_bidan') }}" autofocus>
-                                @error('nama_bidan')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="col-md-12">
-                            <label for="alamat">Alamat</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="far fa-address-card"></i></span>
-                                </div>
-                                <input type="text" class="form-control @error('alamat') is-invalid @enderror" id="alamat" name="alamat" value="{{ old('alamat') }}">
-                                @error('alamat')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="col-md-12">
-                            <label for="spesialis">Spesialis</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="fas fa-id-badge"></i></span>
-                                </div>
-                                <input type="text" class="form-control @error('spesialis') is-invalid @enderror" id="spesialis" name="spesialis" value="{{ old('spesialis') }}">
-                                @error('spesialis')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="col-md-12">
-                            <label for="jd_praktik">Jadwal Praktik</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="far fa-clipboard"></i></span>
-                                </div>
-                                <input type="text" class="form-control @error('jd_praktik') is-invalid @enderror" id="jd_praktik" name="jd_praktik" value="{{ old('jd_praktik') }}">
-                                @error('jd_praktik')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
-                        </div>
-
-
-
-                    </div>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Tambah Data Bidan</h1>
                 </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+                        <li class="breadcrumb-item active">Tambah Bidan</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-              </div>
-              <!-- /.card-body -->
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Tambah Bidan</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form method="post" action="{{ route('bidans.store') }}" class="mb-5" enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-body">
+                                <div class="container">
+                                    <div class="row">
 
-              <div class="card-footer">
-                <button type="submit" class="btn btn-primary">Simpan</button>
-                <button type="reset" class="btn btn-danger">Reset</button>
-              </div>
-            </form>
-          </div>
-          <!-- /.card -->
-        </div>
-      </div>
+                                        <div class="col-md-12">
+                                            <label for="nama_bidan">Nama bidan</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control @error('nama_bidan') is-invalid @enderror" id="nama_bidan" value="{{ Auth::user()->name }}" autofocus disabled>
+                                                <input type="text" class="d-none" id="nama_bidan" name="nama_bidan" value="{{ Auth::user()->name }}">
+                                                <input type="text" class="d-none" id="id_auth" name="id_auth" value="{{ Auth::user()->id }}">
+                                                @error('nama_bidan')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-12">
+                                            <label for="alamat">Alamat</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="far fa-address-card"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control @error('alamat') is-invalid @enderror" id="alamat" name="alamat" value="{{ old('alamat') }}">
+                                                @error('alamat')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-12">
+                                            <label for="spesialis">Spesialis</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="fas fa-id-badge"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control @error('spesialis') is-invalid @enderror" id="spesialis" name="spesialis" value="{{ old('spesialis') }}">
+                                                @error('spesialis')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-12">
+                                            <label for="jd_praktik">Jadwal Praktik</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="far fa-clipboard"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control @error('jd_praktik') is-invalid @enderror" id="jd_praktik" name="jd_praktik" value="{{ old('jd_praktik') }}">
+                                                @error('jd_praktik')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                <button type="reset" class="btn btn-danger">Reset</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
     </section>
 @endsection
--- resources/views/dashboard/bidans/index.blade.php
+++ resources/views/dashboard/bidans/index.blade.php
@@ -1,98 +1,147 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Data Bidan</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+                        <li class="breadcrumb-item active">Data Bidan</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Data Bidan</h1>
+    @if (session()->has('success'))
+        <div class="container-fluid">
+            <div class="alert alert-success col-lg-12" role="alert">
+                {{ session('success') }}
+            </div>
         </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
-            <li class="breadcrumb-item active">Data Bidan</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
+    @endif
 
-  @if(session()->has('success'))
-  <div class="container-fluid">
-    <div class="alert alert-success col-lg-12" role="alert">
-      {{ session('success') }}
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Data Bidan</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            @if (Auth::user()->level == 'admin')
+                                <a href="/dashboard/bidans/create" class="btn btn-primary mb-3 {{ !$cari ? 'd-none' : '' }}"><i class="fas fa-plus-square"></i> Create</a>
+                            @endif
+                            @if (Auth::user()->level == 'bidan')
+                                <a href="/dashboard/bidans/create" class="btn btn-primary mb-3 {{ $cari ? 'd-none' : '' }}"><i class="fas fa-plus-square"></i> Create</a>
+                            @endif
+
+                            <table id="example1" class="table table-bordered table-striped">
+
+                                @if (Auth::user()->level == 'admin')
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nama Bidan</th>
+                                            <th>Alamat</th>
+                                            <th>Spesialis</th>
+                                            <th>Jadwal Praktik</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($bidan_admin as $bidanItem)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $bidanItem->nama_bidan }}</td>
+                                                <td>{{ $bidanItem->alamat }}</td>
+                                                <td>{{ $bidanItem->spesialis }}</td>
+                                                <td>{{ $bidanItem->jd_praktik }}</td>
+                                                <td>
+                                                    <a href="{{ route('bidans.edit', $bidanItem->id) }}" class="btn btn-success d-none"><i class="far fa-edit"></i></a>
+                                                    <form action="{{ route('bidans.destroy', $bidanItem->id) }}" method="post" class="d-inline">
+                                                        @method('delete')
+                                                        @csrf
+                                                        <button class="btn btn-danger border-0" onclick="return confirm('Kamu Yakin?')"><i class="far fa-trash-alt"></i></button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nama Bidan</th>
+                                            <th>Alamat</th>
+                                            <th>Spesialis</th>
+                                            <th>Jadwal Praktik</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot>
+                                @endif
+                                @if (Auth::user()->level == 'bidan')
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nama Bidan</th>
+                                            <th>Alamat</th>
+                                            <th>Spesialis</th>
+                                            <th>Jadwal Praktik</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($bidan as $bidanItem)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $bidanItem->nama_bidan }}</td>
+                                                <td>{{ $bidanItem->alamat }}</td>
+                                                <td>{{ $bidanItem->spesialis }}</td>
+                                                <td>{{ $bidanItem->jd_praktik }}</td>
+                                                <td>
+                                                    <a href="{{ route('bidans.edit', $bidanItem->id) }}" class="btn btn-success"><i class="far fa-edit"></i></a>
+                                                    <form action="{{ route('bidans.destroy', $bidanItem->id) }}" method="post" class="d-inline">
+                                                        @method('delete')
+                                                        @csrf
+                                                        <button class="btn btn-danger border-0" onclick="return confirm('Kamu Yakin?')"><i class="far fa-trash-alt"></i></button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nama Bidan</th>
+                                            <th>Alamat</th>
+                                            <th>Spesialis</th>
+                                            <th>Jadwal Praktik</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot>
+                                @endif
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
     </div>
-  </div>
-  @endif
-  
-<!-- Main content -->
-<section class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12">
-
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Data Bidan</h3>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body">
-            <a href="/dashboard/bidans/create" class="btn btn-primary mb-3"><i class="fas fa-plus-square"></i> Create</a>
-            <table id="example1" class="table table-bordered table-striped">
-              <thead>
-              <tr>
-                <th>#</th>
-                <th>Nama Bidan</th>
-                <th>Alamat</th>
-                <th>Spesialis</th>
-                <th>Jadwal Praktik</th>
-                <th>Action</th>
-              </tr>
-              </thead>
-              <tbody>
-                @foreach ($bidan as $bidanItem)    
-                <tr>
-                  <td>{{ $loop->iteration }}</td>
-                  <td>{{ $bidanItem->nama_bidan }}</td>
-                  <td>{{ $bidanItem->alamat }}</td>
-                  <td>{{ $bidanItem->spesialis }}</td>
-                  <td>{{ $bidanItem->jd_praktik }}</td>
-                  <td>
-                    <a href="{{ route('bidans.edit', $bidanItem->id) }}" class="btn btn-success"><i class="far fa-edit"></i></a>
-                    <form action="{{ route('bidans.destroy', $bidanItem->id) }}" method="post" class="d-inline">
-                      @method('delete')
-                      @csrf
-                    <button class="btn btn-danger border-0" onclick="return confirm('Kamu Yakin?')"><i class="far fa-trash-alt"></i></button>
-                  </form>
-                </td>
-              </tr>
-              @endforeach
-              </tbody>
-              <tfoot>
-              <tr>
-                <th>#</th>
-                <th>Nama Bidan</th>
-                <th>Alamat</th>
-                <th>Spesialis</th>
-                <th>Jadwal Praktik</th>
-                <th>Action</th>
-              </tr>
-              </tfoot>
-            </table>
-          </div>
-          <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
-      </div>
-      <!-- /.col -->
-    </div>
-    <!-- /.row -->
-  </div>
-  <!-- /.container-fluid -->
-</section>
-<!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+    <!-- /.content-wrapper -->
 @endsection
